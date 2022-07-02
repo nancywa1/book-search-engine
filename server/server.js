04608847f4e3b56c00a3cf48878846387e24f125
nancywa1 <nancy.wang0717@gmail.com>
2022-07-02 09:13:03 -0600

    update server.js

--- server/server.js
+++ server/server.js
@@ -8,14 +8,16 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -24,8 +26,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/"));
+// });
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // app.use(routes);
