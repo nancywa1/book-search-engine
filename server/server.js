d8b9fc33d0097cb6ad3fee3ccac6a20948930094
nancywa1 <nancy.wang0717@gmail.com>
2022-07-03 00:46:07 -0600

    middleware error

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 // const db = require("./config/connection");
 // const routes = require("./routes");
-const { authMiddleware } = require("./utils/auth");
+
 const { ApolloServer } = require("apollo-server-express");
 
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -29,9 +30,9 @@ if (process.env.NODE_ENV === "production") {
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/"));
 // });
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // app.use(routes);
 const startApolloServer = async (typeDefs, resolvers) => {
